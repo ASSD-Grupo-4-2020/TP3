63322fafbe3b9620dd14407c77b2d8e4d6318639
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2020-06-04 00:21:20 -0300

    Simulación terminada

--- Python/SigmaDelta.py
+++ Python/SigmaDelta.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from scipy import signal as sign
 import numpy as np
+from numpy.fft import rfft, rfftfreq, ifft
+
 #Sen signal generator
 def EntradaSin(Vmax,t,f):
   x=[]
@@ -35,18 +37,36 @@ def SigmaDelta(X,t):
 def main():
     #Variables
     f=500;
-    L=30;
-    fs=f*25*L;
+    L=64;
+    fs=f*5*L;
     T=1/f;
     t=np.arange(0,1*T+1/fs,1/fs)
     X=EntradaSin(0.8,t,f)
     #Test
     Yn=SigmaDelta(X,t)
+    Yd=sign.decimate(Yn,16,ftype="fir")
+    td=np.arange(0,1*T+16/fs,16/fs)
     plt.plot(t,Yn)
     plt.plot(t,X)
-    Yd=sign.decimate(Yn,L,ftype="fir")
-    td=np.arange(0,1*T+L/fs,L/fs)
-    plt.plot(td,Yd)
+    plt.step(td,Yd)
+    plt.legend(['Cuantizador','Entrada','Decimador'],loc='upper center',shadow=True,ncol=3)
+    plt.title('ADC Sigma delta simulación')
+    plt.ylabel("Tensión [V]")
+    plt.xlabel("Tiempo [Seg]")
+    plt.xlim([0,1*T])
+    plt.ylim([-1.05,1.3])
+    plt.grid(True)
+    plt.show()
+    #Noise shaping
+    freqYn = rfftfreq(len(t), d = t[-1]/len(t))
+    fftYn = rfft(Yn)
+    fftYn1 = 2*np.abs(fftYn/len(t))
+    plt.plot(freqYn,10*np.log10(fftYn1/(10**(-3))))
+    plt.title('Noise shaping')
+    plt.ylabel("Potencia [dBm]")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.xlim([0,80000])
+    plt.grid(True)
     plt.show()
 
 if __name__ == "__main__":
